b9b3fb0a2c1882ad136e83e738f44dda7d8f07fe
Priji <priji.p@gmail.com>
2016-07-06 09:12:24 +0530

    Added Mode 09 PID 01

---
+++ src/main/java/com/github/pires/obd/commands/protocol/AvailablePidsCommand_Mode09_01_20.java
@@ -0,0 +1,29 @@
+package com.github.pires.obd.commands.protocol;
+
+import com.github.pires.obd.enums.AvailableCommandNames;
+
+public class AvailablePidsCommand_Mode09_01_20 extends AvailablePidsCommand {
+
+    /**
+     * Default ctor.
+     */
+    public AvailablePidsCommand_Mode09_01_20() {
+        super("09 00");
+    }
+
+    /**
+     * Copy ctor.
+     *
+     * @param other a {@link com.github.pires.obd.commands.protocol.AvailablePidsCommand} object.
+     */
+    public AvailablePidsCommand_Mode09_01_20(AvailablePidsCommand_Mode09_01_20 other) {
+        super(other);
+    }
+
+    /** {@inheritDoc} */
+	@Override
+	public String getName() {
+		return AvailableCommandNames.PIDS_MODE09_01_20.getValue();
+	}
+
+}
